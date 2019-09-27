63a0fcf2addac823dd238be7a2c32e3fffaf3d39
KushibikiMashu <kushibiki3413@gmail.com>
2019-09-27 12:12:32 +0900

    Add first test

--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -1,8 +1,16 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use App\BowlingGame;
 
 Class BowlingGameTest extends TestCase
 {
-
+    /**
+     * @test
+     */
+    public function 投げる()
+    {
+        $bowlingGame = new BowlingGame;
+        $bowlingGame->roll();
+    }
 }
