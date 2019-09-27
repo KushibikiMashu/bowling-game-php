a8b152ea9d8129577349e7cd3fb7564f44633759
KushibikiMashu <kushibiki3413@gmail.com>
2019-09-27 19:54:48 +0900

    Add second Test

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -4,5 +4,15 @@ namespace App;
 
 Class BowlingGame
 {
+    private $score = 0;
 
+    public function roll(int $pins)
+    {
+        $this->score += $pins;
+    }
+
+    public function score()
+    {
+        return $this->score;
+    }
 }
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -3,14 +3,42 @@
 use PHPUnit\Framework\TestCase;
 use App\BowlingGame;
 
+// 全部ガーター
+// 全部１点
+// 一回スペア
+// 一回ストライク
+// 全部ストライク
+
 Class BowlingGameTest extends TestCase
 {
+    private $bowlingGame;
+
+    public function setUp()
+    {
+        $this->bowlingGame = new BowlingGame;
+    }
+
     /**
      * @test
      */
-    public function 投げる()
+    public function 全部ガーター()
     {
-        $bowlingGame = new BowlingGame;
-        $bowlingGame->roll();
+        for ($i = 0; $i < 20; $i++) {
+            $this->bowlingGame->roll(0);
+        }
+        $actual = $this->bowlingGame->score();
+        $this->assertSame(0, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function 全部一本倒す()
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $this->bowlingGame->roll(1);
+        }
+        $actual = $this->bowlingGame->score();
+        $this->assertSame(20, $actual);
     }
 }
